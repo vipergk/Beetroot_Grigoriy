bd510daded91f4feaedd98afebecc027804e6891
Grigoriy <87998995+vipergk@users.noreply.github.com>
2021-09-03 16:51:33 +0200

    Update mod_hw1.py

--- lesson_19/mod_hw1.py
+++ lesson_19/mod_hw1.py
@@ -1,1 +1,13 @@
+"""Task 1 Imports practice
+Make a directory with 2 modules;
+make a function in one of them;
+then import this function in the other
+module and use that in your script of choice."""
+from mod1.mod1_world import  print_hello_world
+"""imports function "print_hello_world'from directory 'mod1' """
+
+def funtion1():
+    print_hello_world()
+
+funtion1()
 
